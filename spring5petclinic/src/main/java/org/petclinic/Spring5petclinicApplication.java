5e4675ac1756ff8c766f10c7f9a3f2508f300de2
Carlos Alfonso del Rio Juarez <carlos.rio@softtek.com>
2020-01-30 10:09:37 -0600

    adding initial version for spring 5 pet clinic project

---
+++ spring5petclinic/src/main/java/org/petclinic/Spring5petclinicApplication.java
@@ -0,0 +1,13 @@
+package org.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5petclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5petclinicApplication.class, args);
+	}
+
+}
